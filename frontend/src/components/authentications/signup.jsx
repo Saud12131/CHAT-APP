f85ee3728ce2c319dab990493241f73416642113
saud sayyed <155637796+Saud12131@users.noreply.github.com>
2024-09-20 15:41:55 +0530

    bug fixed

--- frontend/src/components/authentications/signup.jsx
+++ frontend/src/components/authentications/signup.jsx
@@ -99,7 +99,8 @@ export const Signup = () => {
           "Content-type": "application/json",
         },
       };
-      const { data } = await axios.post("http://localhost:5000/api/users/signup", { formData },
+      const { data } = await axios.post("http://localhost:5000/api/users/signup",
+        formData,
         config
       );
       settoast({
@@ -119,12 +120,12 @@ export const Signup = () => {
       });
     }
 
-};
-return (
+  };
+  return (
 
-  <div className="container mx-auto min-auto ">
+    <div className="container mx-auto min-auto ">
 
-    <div className="
+      <div className="
     h-[85px] 
     w-[400px]
      ml-[388px] 
@@ -140,9 +141,9 @@ return (
     mx-auto
     min-auto
     bg-slate-50">
-      <h3 className="text-center">Signup</h3>
-    </div>
-    <div className="component
+        <h3 className="text-center">Signup</h3>
+      </div>
+      <div className="component
     container 
     w-[400px] 
     ml-[390px]
@@ -152,120 +153,120 @@ return (
       mt-2
       mb-7
     bg-slate-50">
-      <div className="min-h-screen flex items-center justify-center  pb-6">
-        <form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg shadow-md w-full max-w-sm">
-          <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
-          <div className="mb-4">
-            <label className="block text-gray-700 mb-2" htmlFor="name">
-              name
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={formData.name}
-              onChange={handleChange}
-              placeholder="Enter your name"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              autoComplete='name'
-              required
-            />
-          </div>
+        <div className="min-h-screen flex items-center justify-center  pb-6">
+          <form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg shadow-md w-full max-w-sm">
+            <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
+            <div className="mb-4">
+              <label className="block text-gray-700 mb-2" htmlFor="name">
+                name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                value={formData.name}
+                onChange={handleChange}
+                placeholder="Enter your name"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                autoComplete='name'
+                required
+              />
+            </div>
 
-          <div className="mb-4">
-            <label className="block text-gray-700 mb-2" htmlFor="email">
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="Enter your email"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              autoComplete='email'
-              required
-            />
-          </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 mb-2" htmlFor="email">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="Enter your email"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                autoComplete='email'
+                required
+              />
+            </div>
 
-          <div className="mb-4">
-            <label className="block text-gray-700 mb-2" htmlFor="password">
-              Password
-            </label>
-            <input
-              type={passwordVisib ? 'text' : 'password'}
-              name="password"
-              id="password"
-              value={formData.password}
-              onChange={handleChange}
-              placeholder="Enter your password"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              autoComplete='password'
-              required
-            />
-            <button onClick={togelpass} className='bg-cyan-500 p-1 m-1 rounded-sm color-sky text-slate-50 ' >{passwordVisib ? 'hide' : 'show'}</button>
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 mb-2" htmlFor="confirmPassword">
-              Confirm Password
-            </label>
-            <input
-              type="password"
-              name="confirmPassword"
-              id="confirmPassword"
-              value={formData.confirmPassword}
-              onChange={handleChange}
-              placeholder="Confirm your password"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              autoComplete='confirmpassword'
-            />
-          </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 mb-2" htmlFor="password">
+                Password
+              </label>
+              <input
+                type={passwordVisib ? 'text' : 'password'}
+                name="password"
+                id="password"
+                value={formData.password}
+                onChange={handleChange}
+                placeholder="Enter your password"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                autoComplete='password'
+                required
+              />
+              <button onClick={togelpass} className='bg-cyan-500 p-1 m-1 rounded-sm color-sky text-slate-50 ' >{passwordVisib ? 'hide' : 'show'}</button>
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 mb-2" htmlFor="confirmPassword">
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                name="confirmPassword"
+                id="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                placeholder="Confirm your password"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                autoComplete='confirmpassword'
+              />
+            </div>
 
-          <div className="mb-4">
-            <label className="block text-gray-700 mb-2" htmlFor="pic">
-              Picture
-            </label>
-            <input
-              type="file"
-              name="pic"
-              id="pic"
-              // value={formData.pic[0]}
-              accept='image/*'
-              onChange={(e) => postDetails(e.target.files[0])}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
-            />
-          </div>
-          <button
-            className={`relative inline-flex items-center px-4 py-2 font-semibold text-white bg-blue-600 rounded-md ${loading ? 'opacity-75 cursor-not-allowed' : ''}`}
-            onClick={handleSubmit}
+            <div className="mb-4">
+              <label className="block text-gray-700 mb-2" htmlFor="pic">
+                Picture
+              </label>
+              <input
+                type="file"
+                name="pic"
+                id="pic"
+                // value={formData.pic[0]}
+                accept='image/*'
+                onChange={(e) => postDetails(e.target.files[0])}
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+            </div>
+            <button
+              className={`relative inline-flex items-center px-4 py-2 font-semibold text-white bg-blue-600 rounded-md ${loading ? 'opacity-75 cursor-not-allowed' : ''}`}
+              onClick={handleSubmit}
 
-          >
-            {loading ? (
-              <>
-                <svg
-                  className="animate-spin h-5 w-5 mr-3 border-t-2 border-r-2 border-white rounded-full"
-                  viewBox="0 0 24 24"
-                ></svg>
-                Loading...
-              </>
-            ) : (
-              'Click Me'
+            >
+              {loading ? (
+                <>
+                  <svg
+                    className="animate-spin h-5 w-5 mr-3 border-t-2 border-r-2 border-white rounded-full"
+                    viewBox="0 0 24 24"
+                  ></svg>
+                  Loading...
+                </>
+              ) : (
+                'Click Me'
+              )}
+            </button>
+            <br /><br />
+            {/* Conditionally show the Toast */}
+            {toast.visible && (
+              <Toast
+                type={toast.type}
+                message={toast.message}
+                onClose={() => settoast({ ...toast, visible: false })}
+              />
             )}
-          </button>
-          <br /><br />
-          {/* Conditionally show the Toast */}
-          {toast.visible && (
-            <Toast
-              type={toast.type}
-              message={toast.message}
-              onClose={() => settoast({ ...toast, visible: false })}
-            />
-          )}
-        </form>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
 }
